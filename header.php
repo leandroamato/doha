466459b0d03add2163f6efc854fd2c87949c5880
Leo <leandroamato@gmail.com>
2020-10-15 15:27:28 -0300

    Backwards compatibility: wp_body_open

--- header.php
+++ header.php
@@ -10,7 +10,13 @@
 
 <body <?php body_class(); ?>>
 
-  <?php wp_body_open(); ?>
+  <?php
+  if (function_exists('wp_body_open')) {
+    wp_body_open();
+  } else {
+    do_action('wp_body_open');
+  }
+  ?>
 
   <div id="container">
 
